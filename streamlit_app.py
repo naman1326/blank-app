96a9ef763d63db3bbda8f88eb4b32310efe32bdf
naman1326 <ghodakenaman20@gmail.com>
2024-09-20 16:20:30 +0530

    dashboard_1.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.image('logo.png')
+st.title('Publication Analysis')
